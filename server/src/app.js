b0f91ca3e1c6759921e1fc76a25e33feba97be1c
abhinav <abhinavisgreat18@gmail.com>
2026-06-21 22:24:09 +0530

    updated app.js

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (_, res) => res.send("Hello World!"));
+// app.get("/", (_, res) => res.send("Hello World!"));
 
 app.use("/api", routes);
 
